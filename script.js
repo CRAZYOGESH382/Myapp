cad04e864be7fb59add2004e3e4bfa9dd3a324f3
CRAZYOGESH382 <yogeshhidora@gmail.com>
2025-11-04 17:25:48 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,22 @@
-function sayHi() {
-  alert("Hello Yogesh! 🎉 आपका ऐप अब पूरी तरह से तैयार है 😄");
-  console.log("App Loaded Successfully!");
-}
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDS0qwZFuNE3fR7dDpTz_Sr7NrtqEgAorU",
+  authDomain: "privetchatapp.firebaseapp.com",
+  databaseURL: "https://privetchatapp-default-rtdb.firebaseio.com",
+  projectId: "privetchatapp",
+  storageBucket: "privetchatapp.firebasestorage.app",
+  messagingSenderId: "590135835173",
+  appId: "1:590135835173:web:70d46a34d53af9b2f59dcc",
+  measurementId: "G-J9SMFCJTCR"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
